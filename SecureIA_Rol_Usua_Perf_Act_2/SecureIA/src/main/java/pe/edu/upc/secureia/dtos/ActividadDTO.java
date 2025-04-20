1976842a96374f40bb9cce50fb4106d26a208ae5
Rafa0610 <149858061+Rafa0610@users.noreply.github.com>
2025-04-20 02:59:07 -0500

    Add files via upload
    El segundo avance amplía las tablas Rol y Usuario, incorporando las nuevas entidades Perfil y Actividad. Estas permiten una mayor personalización y seguimiento de acciones dentro del sistema.

---
+++ SecureIA_Rol_Usua_Perf_Act_2/SecureIA/src/main/java/pe/edu/upc/secureia/dtos/ActividadDTO.java
@@ -0,0 +1,58 @@
+package pe.edu.upc.secureia.dtos;
+
+import pe.edu.upc.secureia.entities.Usuario;
+
+import java.time.LocalDate;
+
+public class ActividadDTO {
+
+    private int id_actividad;
+
+    private String tipo_actividad;
+
+    private String descripcion_actividad;
+
+    private LocalDate fechaHora_actividad;
+
+    private Usuario id_usuario;
+
+    public int getId_actividad() {
+        return id_actividad;
+    }
+
+    public void setId_actividad(int id_actividad) {
+        this.id_actividad = id_actividad;
+    }
+
+    public String getTipo_actividad() {
+        return tipo_actividad;
+    }
+
+    public void setTipo_actividad(String tipo_actividad) {
+        this.tipo_actividad = tipo_actividad;
+    }
+
+    public String getDescripcion_actividad() {
+        return descripcion_actividad;
+    }
+
+    public void setDescripcion_actividad(String descripcion_actividad) {
+        this.descripcion_actividad = descripcion_actividad;
+    }
+
+    public LocalDate getFechaHora_actividad() {
+        return fechaHora_actividad;
+    }
+
+    public void setFechaHora_actividad(LocalDate fechaHora_actividad) {
+        this.fechaHora_actividad = fechaHora_actividad;
+    }
+
+    public Usuario getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(Usuario id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+}

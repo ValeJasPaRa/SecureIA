737afcc6d63f8013e06f601e4a1a62ac42deb3a8
DanteDCA <149606981+DanteDCA@users.noreply.github.com>
2025-04-20 18:13:54 -0500

    Add NOTIFICACION and GRABACION tables
    En este sexto avance se incorporaron las tablas NOTIFICACION y GRABACION, las cuales fortalecen el sistema al permitir el registro de alertas generadas por eventos reconocidos y la vinculación de grabaciones asociadas. Estas nuevas entidades complementan la gestión de eventos, mejorando el monitoreo y la trazabilidad de situaciones dentro de los inmuebles.

---
+++ SecureIA_Rol_Usua_Perf_Act_Inmu_Geol_Zona_Dispo_Event_Reco_Grab_Noti_6/src/main/java/pe/edu/upc/secureia/controllers/ActividadController.java
@@ -0,0 +1,51 @@
+package pe.edu.upc.secureia.controllers;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.secureia.dtos.ActividadDTO;
+import pe.edu.upc.secureia.entities.Actividad;
+import pe.edu.upc.secureia.servicesinterfaces.IActividadService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/actividad")
+public class ActividadController {
+
+    @Autowired
+    private IActividadService actS;
+
+    @GetMapping("/listar")
+    public List<ActividadDTO> listar() {
+        return actS.list().stream().map(y->{
+            ModelMapper m = new ModelMapper();
+            return m.map(y, ActividadDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping("/insertar")
+    public void insertar(@RequestBody ActividadDTO u_dto) {
+        ModelMapper m = new ModelMapper();
+        Actividad acti = m.map(u_dto, Actividad.class);
+        actS.insert(acti);
+    }
+
+    @GetMapping("/listarid/{id}")
+    public ActividadDTO buscarId(@PathVariable("id") int id) {
+        ModelMapper m=new ModelMapper();
+        ActividadDTO dto=m.map(actS.listId(id),ActividadDTO.class);
+        return dto;
+    }
+
+    @PutMapping("/modificar")
+    public void modificar(@RequestBody ActividadDTO dto) {
+        ModelMapper m=new ModelMapper();
+        Actividad u=m.map(dto,Actividad.class);
+        actS.update(u);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminar(@PathVariable("id") int id) {actS.delete(id);}
+
+}

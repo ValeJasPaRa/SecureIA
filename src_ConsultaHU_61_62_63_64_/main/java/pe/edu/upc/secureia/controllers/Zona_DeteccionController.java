5231a57c0bc4fdbd7edf5fbd165b65e97caaafcb
ValeJasPaRa <113260336+ValeJasPaRa@users.noreply.github.com>
2025-04-24 09:51:29 -0500

    Add files via upload
    Se agregaron las consultas
    HU61: Buscar usuario por nombre
    HU62: Obtener cantidad de usuarios registrados por mes y año específico
    HU63: Listar cantidad de usuarios agrupados por tipo de rol
    HU64: Obtener inmuebles con menos de tres dispositivos operativos

---
+++ src_ConsultaHU_61_62_63_64_/main/java/pe/edu/upc/secureia/controllers/Zona_DeteccionController.java
@@ -0,0 +1,69 @@
+package pe.edu.upc.secureia.controllers;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.secureia.dtos.CantZonasMonitoriadasDTO;
+import pe.edu.upc.secureia.dtos.CantidadUsuariosxMesyAnioDTO;
+import pe.edu.upc.secureia.dtos.Zona_DeteccionDTO;
+import pe.edu.upc.secureia.entities.Zona_Deteccion;
+import pe.edu.upc.secureia.servicesinterfaces.IZona_DeteccionService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/zona_deteccion")
+public class Zona_DeteccionController {
+
+    @Autowired
+    private IZona_DeteccionService zonaS;
+
+    @GetMapping("/listar")
+    public List<Zona_DeteccionDTO> listar() {
+        return zonaS.list().stream().map(y->{
+            ModelMapper m = new ModelMapper();
+            return m.map(y, Zona_DeteccionDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping("/insertar")
+    public void insertar(@RequestBody Zona_DeteccionDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Zona_Deteccion zon = m.map(dto, Zona_Deteccion.class);
+        zonaS.insert(zon);
+    }
+
+    @GetMapping("/listarid/{id}")
+    public Zona_DeteccionDTO buscarId(@PathVariable("id") int id) {
+        ModelMapper m=new ModelMapper();
+        Zona_DeteccionDTO dto=m.map(zonaS.listId(id),Zona_DeteccionDTO.class);
+        return dto;
+    }
+
+    @PutMapping("/modificar")
+    public void modificar(@RequestBody Zona_DeteccionDTO dto) {
+        ModelMapper m=new ModelMapper();
+        Zona_Deteccion u=m.map(dto,Zona_Deteccion.class);
+        zonaS.update(u);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminar(@PathVariable("id") int id) {zonaS.delete(id);}
+
+    @GetMapping("CantidadZonasMonitoriadasxInmueble")
+    public List<CantZonasMonitoriadasDTO> listarCantZonas() {
+        List<CantZonasMonitoriadasDTO> CantZonasMonitoriadasDTO=new ArrayList<>();
+        List<String[]> fila=zonaS.ZonasconAlertaActiva();
+        for(String[]columna:fila){
+            CantZonasMonitoriadasDTO dto=new CantZonasMonitoriadasDTO();
+            dto.setIdentificarInmueble(Integer.parseInt(columna[0]));
+            dto.setNombre_inmueble(columna[1]);
+            dto.setCantidad_Zonas_Monitoriadas(Double.parseDouble(columna[2]));
+            CantZonasMonitoriadasDTO.add(dto);
+        }
+        return CantZonasMonitoriadasDTO;
+    }
+}

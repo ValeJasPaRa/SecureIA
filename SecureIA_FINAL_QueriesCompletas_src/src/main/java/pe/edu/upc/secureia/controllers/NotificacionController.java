efebacf25d5dec1c540abd26edff59a394ed867e
ValeJasPaRa <113260336+ValeJasPaRa@users.noreply.github.com>
2025-04-25 22:28:58 -0500

    Add files via upload
    Semana 4.1
    Durante esta semana, el grupo 6 ha desarrollado las consultas correspondientes, incorporando un total de 10 consultas orientadas a proporcionar información relevante para la toma de decisiones y la mejora continua de la aplicación.
    Detalladas en el Trello: https://trello.com/b/mQLZNcvm/secureia

---
+++ SecureIA_FINAL_QueriesCompletas_src/src/main/java/pe/edu/upc/secureia/controllers/NotificacionController.java
@@ -0,0 +1,69 @@
+package pe.edu.upc.secureia.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.secureia.dtos.CantidadNotiSegunCategoDTO;
+import pe.edu.upc.secureia.dtos.NotificacionDTO;
+import pe.edu.upc.secureia.entities.Notificacion;
+import pe.edu.upc.secureia.servicesinterfaces.INotificacionService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/notificacion")
+public class NotificacionController {
+
+    @Autowired
+    private INotificacionService notiS;
+
+    @GetMapping("/listar")
+    public List<NotificacionDTO> listar() {
+        return notiS.list().stream().map(y->{
+            ModelMapper m = new ModelMapper();
+            return m.map(y, NotificacionDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping("/insertar")
+    public void insertar(@RequestBody NotificacionDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Notificacion not = m.map(dto, Notificacion.class);
+        notiS.insert(not);
+    }
+
+    @GetMapping("/listarid/{id}")
+    public NotificacionDTO buscarId(@PathVariable("id") int id) {
+        ModelMapper m=new ModelMapper();
+        NotificacionDTO dto=m.map(notiS.listId(id),NotificacionDTO.class);
+        return dto;
+    }
+
+    @PutMapping("/modificar")
+    public void modificar(@RequestBody NotificacionDTO dto) {
+        ModelMapper m=new ModelMapper();
+        Notificacion u=m.map(dto,Notificacion.class);
+        notiS.update(u);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminar(@PathVariable("id") int id) {notiS.delete(id);}
+
+    @GetMapping("/CantidadSegunsuCategoría")
+    public List<CantidadNotiSegunCategoDTO> CantidadxCategoría(@RequestParam String Ingresar_Categoria) {
+        List<CantidadNotiSegunCategoDTO> dtoLista=new ArrayList<>();
+        List<String[]> fila=notiS.CantidadSegunCategoria(Ingresar_Categoria);
+        for(String[]columna:fila){
+            CantidadNotiSegunCategoDTO dto=new CantidadNotiSegunCategoDTO();
+            dto.setNombre_Categoria(columna[0]);
+            dto.setCantidad_segunCategoría(Double.parseDouble(columna[1]));
+            dtoLista.add(dto);
+        }
+        return dtoLista;
+    }
+
+
+
+}
